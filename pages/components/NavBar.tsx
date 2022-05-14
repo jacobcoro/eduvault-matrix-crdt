7ca80a2c737badbdd4b2b8cefc78c1159bcb9931
jacobcoro <xiaokourou@gmail.com>
2022-05-14 23:56:21 +0800

    navbar

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -1,10 +1,19 @@
 import useTheme from './useTheme';
 import styles from './NavBar.module.scss';
+import Link from 'next/link';
 const NavBar = () => {
   const { toggleTheme } = useTheme();
   return (
     <div className={styles.navbar}>
       <button onClick={() => toggleTheme()}>toggle theme</button>
+      <ul className="links" role="navigation">
+        <li>
+          <Link href="/login">Login</Link>
+        </li>
+        <li>
+          <Link href="/">Home</Link>{' '}
+        </li>
+      </ul>
     </div>
   );
 };
