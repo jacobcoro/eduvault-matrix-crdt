08e79ce8022595eb4f3d5f06e6652bf1049b9b44
jacobcoro <xiaokourou@gmail.com>
2022-05-15 08:15:08 +0800

    move components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import NavBar from './components/NavBar';
+import NavBar from '../components/NavBar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
