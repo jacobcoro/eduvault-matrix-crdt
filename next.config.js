12a1040dce21aea76139252938b993341275fb8c
jacobcoro <xiaokourou@gmail.com>
2022-05-15 09:27:18 +0800

    move tests to __tests__ as per next conventions

--- next.config.js
+++ next.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  webpack: (config, { webpack }) => {
-    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /.spec./ }));
-
-    config.node = {
-      fs: 'empty',
-    };
-    // config.module.rules.push({
-    //   test: /\.svg$/,
-    //   use: ['@svgr/webpack'], // if need later
-    // });
-
-    return config;
-  },
-};
+module.exports = {};
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,53 @@
 import { MatrixProvider } from 'matrix-crdt';
+import { MatrixClient } from 'matrix-js-sdk';
+import { useCallback, useState } from 'react';
+type LoginStatus = 'loading' | 'failed' | 'ok' | 'disconnected';
 import styles from './index.module.scss';
 
 const Login = () => {
+  const [matrixProvider, setMatrixProvider] = useState<MatrixProvider>();
+  const [status, setStatus] = useState<LoginStatus>();
+  const onLogin = useCallback(
+    (matrixClient: MatrixClient, roomAlias: string) => {
+      if (matrixProvider) {
+        matrixProvider.dispose();
+        setStatus('disconnected');
+        setMatrixProvider(undefined);
+      }
+
+      const dbs = {
+        user: {
+          id: 'user-room-id',
+          roomInfo: {
+            // find the matrix room info and copy that
+            public: false,
+          },
+        }, // basic user info}
+        notes: {
+          id: 'user-room-id',
+          roomInfo: {
+            public: false,
+          },
+        },
+        flashcards: {
+          id: 'user-room-id',
+          roomInfo: {
+            // find the matrix room info and copy that
+            public: false,
+          },
+        },
+      };
+
+      // (optional) stored on state for easy disconnect + connect toggle
+      // setMatrixClient(matrixClient);
+      // setRoomAlias(roomAlias);
+
+      // actually connect
+      // connect(matrixClient, roomAlias);
+    },
+    [matrixProvider]
+  );
+
   return <div className={styles.root}>Login</div>;
 };
 
