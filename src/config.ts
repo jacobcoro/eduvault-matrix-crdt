d2f3b00323e439364a6556e7f5de7397f41d8950
jacobcoro <xiaokourou@gmail.com>
2022-05-15 23:54:02 +0800

    remove env fallbacks

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,5 @@
-export const TEST_ROOM_ID =
-  process.env.NEXT_PUBLIC_TEST_ROOM_ID ?? 'crdt-test-room:matrix.org';
-export const DEV_USERNAME =
-  process.env.NEXT_PUBLIC_DEV_USERNAME ?? '@your-user-name:matrix.org';
-export const DEV_PASSWORD =
-  process.env.NEXT_PUBLIC_DEV_PASSWORD ?? 'your-password';
+export const TEST_ROOM_ID = process.env.NEXT_PUBLIC_TEST_ROOM_ID ?? '';
+export const DEV_USERNAME = process.env.NEXT_PUBLIC_DEV_USERNAME ?? '';
+export const DEV_PASSWORD = process.env.NEXT_PUBLIC_DEV_PASSWORD ?? '';
 export const MATRIX_SERVER =
   process.env.NEXT_PUBLIC_MATRIX_SERVER ?? 'https://matrix.org';
