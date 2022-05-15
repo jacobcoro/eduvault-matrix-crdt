8334ba834062381db8c07d0486e1b16a196de599
jacobcoro <xiaokourou@gmail.com>
2022-05-15 09:31:55 +0800

    move to src dir

--- __tests__/login.spec.tsx
+++ __tests__/login.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Login from '../pages/login';
+import Login from '../src/pages/login';
 import '@testing-library/jest-dom';
 
 describe('login', () => {
