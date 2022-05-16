a48d309584e52335d66b1a20dd0eb77abe9137b9
jacobcoro <xiaokourou@gmail.com>
2022-05-16 18:07:06 +0800

    fix multiple versions of y issue

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   webpack: (config, { webpack }) => {
     config.resolve.alias.react = path.resolve('./node_modules/react');
-
+    config.resolve.alias.yjs = path.resolve('./node_modules/yjs');
     return config;
   },
 };
