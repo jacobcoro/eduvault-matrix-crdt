3cb76a1b4d47a365f24fda2717e6e6f597092e5c
jacobcoro <xiaokourou@gmail.com>
2022-05-22 13:51:36 +0800

    fix footer login status

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,15 @@ import { useContext, useEffect, useState } from 'react';
 import style from './Footer.module.scss';
 
 const Footer = () => {
-  const { loggedIn, matrixClient } = useContext(StoreContext);
+  const { loggedIn, db } = useContext(StoreContext);
   const [userId, setUserId] = useState('');
   useEffect(() => {
     const getMe = async () => {
-      const res = await matrixClient?.whoami();
+      const res = await db?.matrixClient?.whoami();
       if (res) setUserId(res.user_id);
     };
     getMe();
-  }, [loggedIn, matrixClient]);
+  }, [loggedIn, db, db?.matrixClient]);
   return (
     <footer className={style.root}>
       {userId ? <p>{`signed in as: ${userId}`}</p> : <p>Not signed in</p>}
