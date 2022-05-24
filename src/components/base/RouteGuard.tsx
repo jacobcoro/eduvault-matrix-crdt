536223c287b135c8f692348048f3ad5a09340636
jacobcoro <xiaokourou@gmail.com>
2022-05-24 18:43:54 +0800

    fix route guard

--- src/components/base/RouteGuard.tsx
+++ src/components/base/RouteGuard.tsx
@@ -1,7 +1,7 @@
 import { ConnectStatus } from 'model';
 import { StoreContext } from 'model/storeContext';
 import { useRouter } from 'next/router';
-import { FC, useCallback, useContext, useState } from 'react';
+import { FC, useCallback, useContext, useEffect, useState } from 'react';
 
 const RouteGuard: FC<any> = ({ children }) => {
   const router = useRouter();
@@ -11,26 +11,27 @@ const RouteGuard: FC<any> = ({ children }) => {
     setLoginStatus(status);
   }, []);
 
+  useEffect(() => {
+    const checkForLogin = async () => {
+      // if not logged in, but has localStorage, try to login.
+      const previousLoginData = localStorage.getItem('loginData');
+      if (previousLoginData) {
+        const loginData = JSON.parse(previousLoginData);
+        login(loginData, onSetLoginStatus);
+      } else {
+        // if not logged in, and no localStorage, redirect to login page.
+        router.push('/login');
+      }
+    };
+    if (loginStatus === 'initial') checkForLogin();
+  }, [login, loginStatus, router, onSetLoginStatus]);
+
   if (loginStatus === 'failed') {
     router.push('/login');
     return null;
   }
 
   if (loggedIn || loginStatus === 'ok') return <>{children}</>;
-
-  if (loginStatus === 'loading') return <div>...authenticating</div>;
-
-  // if not logged in, but has localStorage, try to login.
-  const previousLoginData = localStorage.getItem('loginData');
-
-  if (previousLoginData) {
-    const loginData = JSON.parse(previousLoginData);
-    login(loginData, onSetLoginStatus);
-    return <div>...authenticating</div>;
-  } else {
-    // if not logged in, and no localStorage, redirect to login page.
-    router.push('/login');
-    return null;
-  }
+  else return <div>...authenticating</div>;
 };
 export default RouteGuard;
